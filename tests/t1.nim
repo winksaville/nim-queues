fbf619503e154fdd3dfff2307add632812b6bb6e
Wink Saville <wink@saville.com>
2015-04-06 19:28:42 -0700

    Add a simple benchmark.
    times getMsg, addTail, rmvHead and retMsg in a loop. This tests the
    fastest time a single thread and use the thread. Thus it will be
    measure a totally a optimal situation with no contention.

--- tests/t1.nim
+++ tests/t1.nim
@@ -1,42 +1,37 @@
+# The performance comments below where using my Ubuntu linux desktop
+# compiled with nim 0.10.3 sha1: 4b98768a and buildFlags:
+# "-d:release --verbosity:1 --hints:off --warnings:off --threads:on --embedsrc --lineDir:on"
 import msgarena, msgqueue, benchmark
 
-echo "t1.nim"
 
-var
-  ma = newMsgArena()
-  mq1 = newMsgQueue("mq1")
-  msg1 = ma.getMsg(1, 0)
-  msg2 = ma.getMsg(2, 0)
-
-echo "ma=", ma
-echo "mq1", mq1
-
-msg1.next = msg2
-echo "msg1=", msg1, " msg2=", msg2
-
-mq1.addTail(msg1)
-echo "mq1 after adding msg1", mq1
-
-mq1.addTail(msg2)
-echo "mq1 after adding msg2", mq1
-
-suite "bm msgareana", 0.0:
+# Having the warmup between 0 .. 0.25 seems to work best.
+# With a warmup of 1.0 the min was 258cy!
+suite "bm msgareana", 0.25:
   var
     ma: MsgArenaPtr
+    mq: MsgQueuePtr
     msg: MsgPtr
-    ts: TestStats
+    tsa: array[0..4, TestStats]
+
+  # For some reason doing this newMsgArena gives
+  # more consistent results. A minimum of 244cy
+  # and "good minC" at 253cy very consistently
+  # when running tests/t1. Not doing this I see
+  # min's of 259..262cy and 277cy with a "good
+  # minC". This maybe related to the fact that
+  # newMsgArena warms up the caches better as
+  # it does some allocShared0 call
+  discard newMsgArena()
 
   setup:
     ma = newMsgArena()
-    echo "setup: ma=", ma
+    mq = newMsgQueue("mq")
   teardown:
-    echo "teardown: ma=", ma
+    mq.delMsgQueue()
     ma.delMsgArena()
 
-  test "test1", 1, ts:
+  test "test1", 1.0, tsa:
     msg = ma.getMsg(1, 0)
-    echo "ma=", ma
-    echo "msg=", msg
+    mq.addTail(msg)
+    msg = mq.rmvHead
     ma.retMsg(msg)
-    echo "ma=", ma
-    echo "msg=", msg
